87cc302f271e08ab75770f1161424416a66ead8c
John Garbero <john.garbero.itb@gmail.com>
2022-05-20 10:54:43 -0500

    commit tree 1bd4d8853ff041a1d8dcbd9da3aa322a711d0298

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
+// const input = document.getElementById('input');
+
 function addingEventListener() {
+    const input = document.getElementById('input');
+    function clickAlert() {
+        alert('I was clicked');
+    }
+    input.addEventListener('click', clickAlert);
+    
 }
+
+
+// input.addEventListener('click', clickAlert);
+
+
+const input = document.getElementById('input');
+input.addEventListener('click', addingEventListener());
+
+// const input = document.getElementById('input');
+
+// function addingEventListener() {
+//     alert('I was clicked!')
+// }
+
+// // input.addEventListener('click', clickAlert);
+
+// const input = document.getElementById('input');
+// input.addEventListener('click', addingEventListener);
+
+// function addingEventListener() {
+//     const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
+// }
+
+// addingEventListener();
